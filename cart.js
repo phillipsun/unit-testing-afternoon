5653dd6fb642d6f66b6c9d0dbe48cb57869f7993
Phillip Sun <psun.04@gmail.com>
2018-08-11 12:10:12 -0500

    used tests to created solutions to each method & property

--- cart.js
+++ cart.js
@@ -1,18 +1,29 @@
 const cars = require('./data/cars');
 
 module.exports = {
-  cart: null,
-  total: null,
+  // set initial state of cart to empty array
+  // and set total to 0
+  cart: [],
+  total: 0,
 
-  addToCart: function() {
-
+  addToCart: function (car) {
+    // add car to cart
+    this.cart.push(car);
+    // add price of car to cart total
+    this.total += car.price;
   },
 
-  removeFromCart: function() {
-
+  removeFromCart: function (index, price) {
+    // remove item from cart array
+    this.cart.splice(index, 1);
+    // subtract item from cart total  
+    this.total -= price;
   },
-  
-  checkout: function() {
 
+  checkout: function () {
+    // empty cart array
+    // and set total to 0
+    this.cart = [];
+    this.total = 0;
   }
 };
